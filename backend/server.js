85b1f6745777e0ce421798f608e833494459a86f
Namanl2001 <namanlakhwani@gmail.com>
2020-08-28 17:19:35 +0530

    final check

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static("client/build"));
 
 const url =
+  process.env.MONGODB_URI ||
   "mongodb+srv://naman:naman@12345@cluster0.44im5.mongodb.net/project?retryWrites=true&w=majority";
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
@@ -26,5 +28,9 @@ app.use("/users", usersRouter);
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/build"));
+}
 
 // alert("server running");
