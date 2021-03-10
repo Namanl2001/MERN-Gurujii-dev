1038ba513d172cc9f341128bee944c0a02c440d5
vtejaeta <vtejaeta@gmail.com>
2021-03-10 17:54:40 +0530

    refactored connection of mongodb to avoid some errors

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); // bodyParser is deprecated and can be avoided
 const path = require("path");
-const { username, pass, myCluster } = require('./config');
 require("dotenv").config();
+const usersRouter = require("./routes/users");
+const { username, pass, myCluster } = require("./config");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// these can abe avoided beacuse of deprecation of bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,19 +20,34 @@ app.use(express.json());
 app.use("/", express.static("./public"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const url =
-  `mongodb+srv://${username}:${pass}${myCluster}.mongodb.net/project?retryWrites=true&w=majority`;
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+const url = `mongodb+srv://${username}:${pass}${myCluster}.mongodb.net/mern-guruji?retryWrites=true&w=majority`;
 
-const usersRouter = require("./routes/users");
+// mongoose.connect(url, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
+
+// for handling connection errors, use try-catch with async/await.
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB connected ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.use("/users", usersRouter);
 
