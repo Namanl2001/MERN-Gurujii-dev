9a343548db0b622e2ab2022560f3a5c2887157cc
vtejaeta <vtejaeta@gmail.com>
2021-03-10 17:57:02 +0530

    added timestamps in user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,86 +1,92 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  subject: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  coaching: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  qualification: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  about: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  class1: {
-    type: Number,
-    min: 9,
-    max: 12,
-    trim: true,
-    required: true,
-  },
-  class2: {
-    type: Number,
-    min: 9,
-    max: 12,
-    trim: true,
-  },
-  class3: {
-    type: Number,
-    min: 9,
-    max: 12,
-    trim: true,
-  },
-  class4: {
-    type: Number,
-    min: 9,
-    max: 12,
-    trim: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    subject: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    coaching: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    class1: {
+      type: Number,
+      min: 9,
+      max: 12,
+      trim: true,
+      required: true,
+    },
+    class2: {
+      type: Number,
+      min: 9,
+      max: 12,
+      trim: true,
+    },
+    class3: {
+      type: Number,
+      min: 9,
+      max: 12,
+      trim: true,
+    },
+    class4: {
+      type: Number,
+      min: 9,
+      max: 12,
+      trim: true,
+    },
 
-  address: {
-    type: String,
-    trim: true,
-    required: true,
+    address: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    city: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    pin: {
+      type: Number,
+      min: 100000,
+      max: 999999,
+      trim: true,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      min: 1000000000,
+      max: 9999999999,
+      trim: true,
+      required: true,
+    },
   },
-  city: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  pin: {
-    type: Number,
-    min: 100000,
-    max: 999999,
-    trim: true,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    min: 1000000000,
-    max: 9999999999,
-    trim: true,
-    required: true,
-  },
-});
+  {
+    // this includes time at which this is created
+    timestamps: true, // sorting can be done by time
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
