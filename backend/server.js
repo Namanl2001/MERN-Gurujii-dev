7c5a448653713e819b2c198a78388785769df75f
ABHAY CHAUHAN <42849043+abhay27chauhan@users.noreply.github.com>
2021-03-15 01:28:55 +0530

    removed body-parser (#77)
    body-parser implementation is now included in the default Express package

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const path = require("path");
 const { username, pass, myCluster } = require('./config');
 require("dotenv").config();
@@ -9,11 +8,10 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
-app.use(express.json());
 // app.use(express.static(path.join(__dirname, "client", "build")));
 app.use("/", express.static("./public"));
 app.use(express.static(path.join(__dirname, "public")));
