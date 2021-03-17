cdc9108b5cd01806f7a88f5f34cf7e8769d915ac
ABHAY CHAUHAN <42849043+abhay27chauhan@users.noreply.github.com>
2021-03-18 03:57:35 +0530

    promises handled successfully and config folder created for database connection (#87)

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (url) => {
+    try{
+        const conn = await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    }catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const { username, pass, myCluster } = require('./config');
 require("dotenv").config();
+const connectDB = require("./config/db")
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,11 +19,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const url =
   `mongodb+srv://${username}:${pass}${myCluster}.mongodb.net/project?retryWrites=true&w=majority`;
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+
+connectDB(url);
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
