21462f950322bcaab5b3610931579eea8d71b9af
Uday Mittal <mittaluday435@gmail.com>
2021-03-19 17:24:14 +0530

    Added various security features (#79)

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,59 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 const { username, pass, myCluster } = require('./config');
-require("dotenv").config();
-const connectDB = require("./config/db")
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
+app.use(express.json());
 // app.use(express.static(path.join(__dirname, "client", "build")));
-app.use("/", express.static("./public"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use('/', express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const url =
-  `mongodb+srv://${username}:${pass}${myCluster}.mongodb.net/project?retryWrites=true&w=majority`;
+// set HTTP security headers
+app.use(helmet());
 
-connectDB(url);
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+//limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many request from this IP, please try again after an hour!',
 });
 
-const usersRouter = require("./routes/users");
+app.use('/', limiter);
 
-app.use("/users", usersRouter);
+//data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+//data sanitization against xss
+app.use(xss());
+
+const url = `mongodb+srv://${username}:${pass}${myCluster}.mongodb.net/project?retryWrites=true&w=majority`;
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
