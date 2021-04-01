d17857d18485b1f4d7fcbacb58d490edbe6857bd
Yash <khareyash05@gmail.com>
2021-04-01 19:37:24 +0530

    add dark mode

--- src/App.js
+++ src/App.js
@@ -6,30 +6,39 @@ import ChatBotComponent from './components/chatbot';
 import './App.css';
 import Board from './board.jpg';
 import Footer from './components/Footer';
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './components/useDarkMode';
+import Toggle from './components/Toggle';
+import { lightTheme, darkTheme } from './components/themes';
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
     <>
-    <Router>
-      <div className='parallax'>
-        <Navbar />
-        <br />
-        <br />
-        <br />
-        <div className='center'>
-          <img src={Board} alt='' />
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
-
-      <br />
-      <Route path='/' exact component={Home} />
-      <ChatBotComponent />
-    </Router>
-    <Footer/>
+      <Router>
+        <ThemeProvider theme={themeMode}>
+          <div className='parallax'>
+            <Navbar />
+            <br />
+            <br />
+            <br />
+            <div className='center'>
+              <img src={Board} alt='' />
+              <Toggle theme={theme} toggleTheme={toggleTheme} />
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+          <br />
+          <Route path='/' exact component={Home} />
+        </ThemeProvider>
+        <ChatBotComponent />
+      </Router>
+      <Footer />
     </>
   );
 }
---
+++ src/components/themes.js
@@ -0,0 +1,9 @@
+export const lightTheme = {
+  body: '#fff',
+  text: '#121212',
+};
+
+export const darkTheme = {
+  body: '#121212',
+  text: '#fff',
+};
