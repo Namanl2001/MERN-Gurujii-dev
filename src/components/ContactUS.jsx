b92d54c4d132ddc09c01cd5bf602e368c712cb2a
satyampgt4 <satyamgpt44@gmail.com>
2021-04-07 22:15:35 +0530

    contact form ui

--- src/components/ContactUS.jsx
+++ src/components/ContactUS.jsx
@@ -5,17 +5,15 @@ function ContactUS() {
   return (
     <>
       <div class='container contct'>
-        <h1>Contact Us</h1>
         <div class='row contct'>
           <form id='contact-form' class='card'>
-            <input type='hidden' name='contact_number' />
-            <label>Name</label>
-            <input type='text' name='user_name' />
-            <label>Email</label>
-            <input type='email' name='user_email' />
-            <label>Message</label>
-            <textarea name='message'></textarea>
-            <input type='submit' value='Send' />
+            <h1>
+              <strong>Contact us</strong>
+            </h1>
+            <input type='text' name='user_name' placeholder='Name' />
+            <input type='email' name='user_email' placeholder='Email' />
+            <textarea name='message' placeholder='Massage'></textarea>
+            <input type='submit' value='Send' id='form-submit' />
           </form>
         </div>
       </div>
