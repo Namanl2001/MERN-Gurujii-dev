3e2fd88621a8ab872f419c9900f28a3b4431520a
Mitali Sonawane <61057219+mitalirs@users.noreply.github.com>
2021-04-23 15:41:33 +0530

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -51,7 +51,7 @@ class form extends Component {
         pin: this.state.pin,
         phone: this.state.phone,
       })
-      .then((response) => {
+      .then(response => {
         if (response.status === 200) {
           axios.get(`/users/sendMail/${this.props.emailid}/1`);
           if (
