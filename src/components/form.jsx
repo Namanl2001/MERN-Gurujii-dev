d51a2f2deb902a721f875d25f8d91a6c46668fdd
theBeginner86 <tobblex@hotmail.com>
2021-05-05 12:44:50 +0530

    Improved form-responsiveness

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -218,7 +218,7 @@ class form extends Component {
                       value={this.state.c1}
                     />
                   </Col>
-                  <Form.LabelCol col='sm-2' htmlFor='c2'>
+                  <Form.LabelCol col='sm-3' htmlFor='c2'>
                     Class 2
                   </Form.LabelCol>
                   <Col col='sm-3'>
@@ -245,7 +245,7 @@ class form extends Component {
                       value={this.state.c3}
                     />
                   </Col>
-                  <Form.LabelCol col='sm-2' htmlFor='c2'>
+                  <Form.LabelCol col='sm-3' htmlFor='c2'>
                     Class 4
                   </Form.LabelCol>
                   <Col col='sm-3'>
