5b8ae12ad78d9a7a8a274fbfe8b888036d40fb1b
Ankita <ankitapal7777@gmail.com>
2021-05-05 18:53:30 +0530

    navbar updated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -56,6 +56,7 @@ class home extends Component {
           <br />
           <br />
           <br />
+          <br />
           <div className='center'>
             <img src={Board} alt='' />
           </div>
