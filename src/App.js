0a12a94ee3c523c2b82bb3ce1fe8825b97a6f1c7
Chitvan Ramani <chitvanramani@gmail.com>
2021-05-10 12:43:20 +0530

    splash screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/home';
+import Preloader from './components/Preloader';
 import './App.css';
 import Footer from './components/Footer';
 import E404 from './components/E404';
+const Home = React.lazy(() => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(import('./components/home')), 7000);
+  });
+});
 
 function App() {
   return (
     <>
       <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='*' component={E404} />
-        </Switch>
+        <React.Suspense fallback={<Preloader />}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='*' component={E404} />
+          </Switch>
+        </React.Suspense>
       </Router>
       <Footer />
     </>
