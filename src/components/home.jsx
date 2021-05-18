add39e96cca5d7d1cbc7158b7aa7e6dcfc680079
ChitvanRamani22 <68965499+ChitvanRamani22@users.noreply.github.com>
2021-05-18 19:07:53 +0530

    social media sidebar added (#195)

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,6 +5,11 @@ import Card from './card';
 import Filter from './filter';
 import { Container, Row } from 'bootstrap-4-react';
 import { Progress } from 'bootstrap-4-react';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import ScrollToTop from './scrollToTop';
 import Board from '../assets/board.jpg';
 import Navbar from '../components/navbar';
@@ -93,6 +98,40 @@ class home extends Component {
             >
               <Row>{this.List()}</Row>
             </Container>
+            <div className='social'>
+              <ul>
+                <li>
+                  <a href='/#'>
+                    <span>WhatsApp</span>
+                    <WhatsAppIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href='/#'>
+                    <span>Facebook</span>
+                    <FacebookIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href='https://github.com/Namanl2001/MERN-Gurujii-dev'>
+                    <span>Github</span>
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href='/#'>
+                    <span>Instagram</span>
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href='/#'>
+                    <span>LinkedIn</span>
+                    <LinkedInIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
             <ScrollToTop />
             <Vision />
             <Reach />
